63b4366aedc3aa08718caa1f54f692e47179b0d5
Vinay chauhan <vchauhan765@rku.ac.in>
2022-02-22 12:33:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
